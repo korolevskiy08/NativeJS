38bdf3aeb0d735a6004b1f9214206ea26049b016
korolevskiy08 <emkorolev12@gmail.com>
2022-05-25 22:29:46 +0300

    first commit

---
+++ Native.js
@@ -0,0 +1,40 @@
+// const user = JSON.stringify(users)
+//
+// const newUser = {...users}
+//
+// const realCopyUser = {}
+// realCopyUser.name = users.name
+// realCopyUser.age = users.age
+// realCopyUser.isMarried = users.isMarried
+
+const user = {
+    name: "Bob",
+    age: 26,
+    isMarried: true,
+    friends: ["Alex", "Bob", "John"],
+    address: {
+        city: "Boborujsk",
+        street: "Lenina"
+    }
+}
+
+
+let copyUser = structuredClone(user);
+
+const students = [
+    {name: "Bob"},
+    {name: "Alex"},
+    {name: "Ann"}
+]
+
+const deepCopyStudents = students.map(student => ({...student}))
+
+let copyStudents = structuredClone(students)
+
+console.log(copyUser)
+console.log(copyStudents)
+console.log(deepCopyStudents)
+console.log(user.friends === copyUser.friends)
+console.log(user.address === copyUser.address)
+
+
